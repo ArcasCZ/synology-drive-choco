4721ebb65b2c832f7a1bd4ce4559ad8a5a30e09d
Vojtěch Olej <Vojtech.Olej@logex.com>
2024-01-12 13:48:28 +0100

    Update app version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'Stop'
   
 $packageName = 'synologydrive'
-$url         = 'https://global.synologydownload.com/download/Utility/SynologyDriveClient/3.3.0-15082/Windows/Installer/Synology%20Drive%20Client-3.3.0-15082.msi?model=RS1219%2B&bays=8&dsm_version=7.2.1&build_number=69057'
-$checksum    = '692a28d4e6f572a2a0e42edddd228f0e355e9926837020c46374aabaf2a60d85'
+$url         = 'https://global.synologydownload.com/download/Utility/SynologyDriveClient/3.4.0-15724/Windows/Installer/i686/Synology%20Drive%20Client-3.4.0-15724-x86.msi'
+$checksum    = '5afeda72ca10a936df98a61ba2b54f8c1d1e767cf6e6d45382b24fd18ec77f10'
   
 $packageArgs = @{
   packageName            = $packageName
