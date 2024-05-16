dfc3a32acb18437e88639fb5cf594d090f8721ba
Vojtěch Olej <Vojtech.Olej@logex.com>
2024-05-16 12:19:22 +0200

    Revert to x86 version (x64 is broken)

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'Stop'
   
 $packageName = 'synologydrive'
-$url         = 'https://global.synologydownload.com/download/Utility/SynologyDriveClient/3.5.0-16084/Windows/Installer/x86_64/Synology%20Drive%20Client-3.5.0-16084-x64.msi'
-$checksum    = '5fc1eb8af607fab0e77add9a5667ee60a10541cdc26c652cb292da484ade09f5'
+$url         = 'https://global.synologydownload.com/download/Utility/SynologyDriveClient/3.5.0-16084/Windows/Installer/i686/Synology%20Drive%20Client-3.5.0-16084-x86.msi'
+$checksum    = '0ad1850446ec23258bc56cd5f8b5170dac3db9f166923d9e5c4f2b221683b574'
   
 $packageArgs = @{
   packageName            = $packageName
