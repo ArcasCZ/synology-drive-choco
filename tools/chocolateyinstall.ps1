03dda35a3420d3fcd32688dcb24bc9a368b14de1
Vojtěch Olej <Vojtech.Olej@logex.com>
2025-01-08 10:32:33 +0100

    Update to 3.5.1.16102

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@
 $ErrorActionPreference = 'Stop'
   
 $packageName = 'synologydrive'
-$url         = 'https://global.synologydownload.com/download/Utility/SynologyDriveClient/3.5.0-16084/Windows/Installer/i686/Synology%20Drive%20Client-3.5.0-16084-x86.msi'
-$checksum    = '0ad1850446ec23258bc56cd5f8b5170dac3db9f166923d9e5c4f2b221683b574'
+$url         = 'https://global.synologydownload.com/download/Utility/SynologyDriveClient/3.5.1-16102/Windows/Installer/x86_64/Synology%20Drive%20Client-3.5.1-16102-x64.msi'
+$checksum    = '35f35849f0fc64a04b01dd7332a15662cc172e00c3332f0ac62bebe8582c68ce'
   
 $packageArgs = @{
   packageName            = $packageName
